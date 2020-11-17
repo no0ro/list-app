9abb558cac3d77b153a67d475ae2b587f2bc8098
no0ro <nrolwes@gmail.com>
2020-11-17 08:38:39 -0500

    added index action

--- list-backend/app/controllers/lists_controller.rb
+++ list-backend/app/controllers/lists_controller.rb
@@ -2,5 +2,7 @@ class ListsController < ApplicationController
     def index
         lists = List.all
         render json: lists
+        # render json: {status: 'SUCCESS', message: 'Loaded all posts', data: lists}, status: :ok
+
       end
 end
