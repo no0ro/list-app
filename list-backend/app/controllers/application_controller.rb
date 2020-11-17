1d7dc149986c704e64d83edc6cd42c73a2900572
no0ro <nrolwes@gmail.com>
2020-11-16 19:25:01 -0500

    added test code to check that back and frontend were communicating

--- list-backend/app/controllers/application_controller.rb
+++ list-backend/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+
+
+    
+#  test that we can get data from the backend
+    # def test
+    #     render json: { test: "success" }
+    #   end
 end
--- list-backend/config/routes.rb
+++ list-backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :lists, only: [:create, :index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # test that we can get data from the backend
+  # get '/test', to: 'application#test'
 end
