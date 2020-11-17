e2a46f113b1177d4b07112375a9088f700d9dec9
no0ro <nrolwes@gmail.com>
2020-11-17 03:42:41 -0500

    vertically build out List - Read

--- list-backend/app/controllers/lists_controller.rb
+++ list-backend/app/controllers/lists_controller.rb
@@ -1,2 +1,6 @@
 class ListsController < ApplicationController
+    def index
+        lists = List.all
+        render json: lists
+      end
 end
--- list-frontend/src/index.js
+++ list-frontend/src/index.js
@@ -4,3 +4,45 @@
 //   .then(response => response.json())
 //   .then(parsedResponse => console.log(parsedResponse));
 
+const BACKEND_URL = 'localhost:3000';
+
+
+function getLists() {
+    return fetch('http://localhost:3000/lists')
+      .then(res => res.json())
+  } 
+
+  // render the card skeleton - div card / ul
+  function renderList(list) {
+    let li = document.createElement('li')
+    li.innerText = list.title
+    li.setAttribute('class', 'list-group-item')
+
+    let ul = document.createElement('ul')
+    ul.setAttribute('class', 'list-group')
+    ul.setAttribute('class', 'list-group-flush')
+
+
+// insert Item here
+
+// delete button (for each item )
+    // let btn = document.createElement('button')
+    // btn.setAttribute('class', 'delete-btn')
+    // btn.setAttribute('id', item.id)
+    // btn.innerText = "Delete"
+    // btn.addEventListener('click', (e) => {
+    //   console.log(e.target.dataset);
+    //   delete_meth(e)
+    // })
+    let divListCollection = document.getElementById('list-collection')
+   
+    ul.append(li ) // change to TitleLi / chnge formatting so li is diff than title via bootstrap
+    divListCollection.append(ul)
+  }
+  
+
+getLists().then(lists => {
+    lists.forEach(list => {
+        renderList(list)
+    })
+})
