cb0a4ef0ff7128eadfae8dc8bfbe635de38526f8
no0ro <nrolwes@gmail.com>
2020-11-17 03:40:21 -0500

    added List seeds

--- list-backend/db/seeds.rb
+++ list-backend/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+l1 = List.create(title: "React Project Ideas")
+l2 = List.create(title: "Faith's Wedding: Maid of Honor Duties")
+l3 = List.create(title: "Grocery List")
+l4 = List.create(title: "Faith's Wedding: Bachelorette Party Planning")
+l5 = List.create(title: "Movies To Watch")
+
+
+#  List.create(title: "Gratitude List") / albums to listen to/ quotes
