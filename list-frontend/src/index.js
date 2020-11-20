0dbb8cbf0f461f2b00e5d1a3a14c50ab67186b40
no0ro <nrolwes@gmail.com>
2020-11-20 09:56:56 -0500

    added fetch lists fn

--- list-frontend/src/index.js
+++ list-frontend/src/index.js
@@ -2,25 +2,20 @@ console.log('index.js - FIRST LINE')
 
 document.addEventListener('DOMContentLoaded', () =>  {
     getLists();
-    
-  
 
-    const list = new List()
-    // list.title = "HI" // an object of, that has ListAdapter{} obj in it. so skips the List{} step up above. 
-    console.log(list)
-
- 
-    list.getFetchAndMakeLists()
-    // console.log(list.getFetchAndMakeLists()) // if try to replace "list" with "app" says getFetchAndMakeLists() is not a fn 
-        // NOTE: when getLists returns the Response Object from fetch, it gets saved in Promise Obj's `value:` property. this is what triggers Promise's onFulfilled:[ `aka .then(fn)`] to move to microqueue 
-        // so now that have data back from fetch, js calls it on list... then iterate from "inside RenderList" to "hi"..5 times
-        console.log('back in listener')
    
 }) 
 
+// const list = new List()
+//     // list.title = "HI" // an object of, that has ListAdapter{} obj in it. so skips the List{} step up above. 
+//     console.log(list)
 
-
-
+ 
+//     list.getFetchAndMakeLists()
+//     // console.log(list.getFetchAndMakeLists()) // if try to replace "list" with "app" says getFetchAndMakeLists() is not a fn 
+//         // NOTE: when getLists returns the Response Object from fetch, it gets saved in Promise Obj's `value:` property. this is what triggers Promise's onFulfilled:[ `aka .then(fn)`] to move to microqueue 
+//         // so now that have data back from fetch, js calls it on list... then iterate from "inside RenderList" to "hi"..5 times
+//         console.log('back in listener')
 
 
 
@@ -41,31 +36,4 @@ getLists().then(lists => {
     this.renderList(list)
     })
 })
-
-
---> 2nd Way: 
-
-# ListAdapter.js 
-    getLists() {
-        return fetch(this.baseURL)
-          .then( response => response.json()) // returns array of objects
-          // .then( data => data) // returns array of objects
-    }
-
-# List.js - Component
-    constructor(title) {
-        this.adapter = new ListAdapter()
-        // this.title = title
-    }
-
-    getFetchAndMakeLists() {
-        let json = this.adapter.getLists() // returns arr of list obj's
-          .then(arrOfListObjects => {  
-              arrOfListObjects.forEach(listObject => {
-              this.renderList(listObject) 
-      })
-    })
-
-- @ .then(arrOfList...) is when i called getLists() in first way 
--  dont think i need the line .then( data => data) // returns array of objects
 */
--- list-frontend/src/list.js
+++ list-frontend/src/list.js
@@ -1,3 +1,8 @@
+const listForm = ` 
+    <label for="list-title">List Title:</label>
+    <input type="text" name="list-title" placeholder="List Title" value=""  class="input-text"/>
+`
+
 class List {
     constructor(title) {
         this.title = title
@@ -5,42 +10,41 @@ class List {
         this.lists = []
     }
 
-    getLists() {
-        console.log("inside getLists()")
 
-         return fetch("http://localhost:3000/lists")
-            .then(response => response.json()) // .then puts the responce fn inside the onFulfilled:[] array property. thats it! 
-            .then(json => (json.data))
+}
+
+function getLists() {
+    console.log("inside getLists()")
+
+    fetch("http://localhost:3000/lists")
+        .then(response => response.json()) // .then puts the responce fn inside the onFulfilled:[] array property. thats it! 
+        .then(json => (json.data))
+        .then(data => {
+            console.log(data)
+            renderList(data)
+        } )
             // .then(data => {
             //     console.log(data[1].attributes.title) // "Faith's Wedding: MOH"
             //     console.log(data[1].attributes.items[0].name) // "Plan Bachelorette"
 
             // })
             // ^ returns array of objects
-            .catch(console.error)
-    }
+        .catch(console.error)
+}
 
-    getFetchAndMakeLists() {
-        console.log("inside getfetchWndmakelists - list component")
-        this.adapter.getLists()
-            .then(lists => this.renderLists(lists))  
-            .then(() => this.appendLists())
-            // -> render list html 
-            // 
-    }
+   
 
-
-    // JUST making lots of <li> -- call this inside of renderList?
-    renderItems() {
+// JUST making lots of <li> -- call this inside of renderList?
+function renderItems() {
         return this.items.map((item) => item.render()).join("");
+}
+
+
+    function renderAList(){
+
     }
 
-
-    renderAList(){
-
-    }
-
-    appendLists(){
+    function appendLists(){
         for (let list of lists){
             console.log("append list")
         }
@@ -48,7 +52,7 @@ class List {
 
 
 
-    makeArrOfItems(items){
+    function makeArrOfItems(items){
         let itemsArr = []
         for (let item of items){
             itemsArr.push(item.name)
@@ -56,12 +60,12 @@ class List {
         return itemsArr
     }
 
-    createNewItem(name){
+    function createNewItem(name){
         const newItem = new Items(); // instanciate new Ite.],
         this.items.push(newItem) // push item into items array
     }
 
-    renderLists(lists){
+    function renderLists(lists){
         console.log("in renderLists")
         for (let list of lists){
             //let title = this.attributes.title
@@ -81,7 +85,7 @@ class List {
     // ^^ goal? render the card skeleton - div card / ul
             // Next, call & iterate addItems() + add listener to delete button
             // addItems / renderItems should be in Item component
-    renderList(arrOfListObjects) {
+function renderList(arrOfListObjects) {
         console.log("inside renderList- list component")
         console.log(arrOfListObjects)
 
@@ -121,146 +125,5 @@ class List {
     
     })
 } 
-}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // <div class="card-body">
-//         let cardBodyDiv = document.createElement('div')
-//         cardBodyDiv.setAttribute('class', 'card-body')
-
-//     // <h5 class="card-title">Card title</h5>
-//         let cardTitle = document.createElement('h5')
-//         cardTitle.setAttribute('class', 'card-title')
-//         cardTitle.innerText = listObject.title
-//     // append ^ 
-//         cardBodyDiv.append(cardTitle)
-
-
-// // <ul class="list-group list-group-flush">
-//         // Item Display - Ul container 
-//         let itemUl = document.createElement('ul')
-//         itemUl.setAttribute('class', 'list-group')
-//         itemUl.setAttribute('class', 'list-group-flush')
-
-//     // <li class="list-group-item">Cras justo odio</li>
-//         // Item Display - Li
-//         let itemLi = document.createElement('li')
-//         itemLi.innerText = ?listObject?.name
-//         itemLi.setAttribute('class', 'list-group-item')
-
-//     // append them 
-//         itemUl.append(itemLi)
-
-// // append all 
-
-
-
-
-
-
-
-// Think it through
-// renderItems() {
-        // pass in Array of items, then iterate over and call 
-        // render() (defined in items component) on each arr item. 
-        // aka turn user input into html/bootstrap
-    // }
-    // insert Item here
-    
-    // delete button (for each item )
-        // let btn = document.createElement('button')
-        // btn.setAttribute('class', 'delete-btn')
-        // btn.setAttribute('id', item.id)
-        // btn.innerText = "Delete"
-        // btn.addEventListener('click', (e) => {
-        //   console.log(e.target.dataset);
-        //   delete_meth(e)
-        // })
-
-// the name is in the same place as the behavior 
-// - list.title // Grocery Shopping 
-// - list.push(newItem) // list can add more items to itself  via button after its alrady been created 
-// - item.hightlight() // can be highlighted 
-
-// - item.description
-// - item.deleted
-// - marked urgent! aka change color
-// // - has a title & description 
-// - 
-
-
-
-// possible Functions
-
-    // static formHTML(list){
-    //     return()
-    // }
-    // get formHTML(){
-    //     return Location.formHTML(this)
-    // }
-
-    // create Item 
-        // description 
-        // X  button to delete 
-
-    // Render Items
-            // createNewItem() {
-
-            //}
-  
-         
-// _______________________________________
-// differemt way to render HTML
-    // html(){
-    //     return(`
-    //         <div class="card-body">
-    //           <h5 class="card-title">${this.title}</h5>  
-    //             <ul class="list-group list-group-flush">
-    //                 ${this.createItemLis()}
-    //             </ul>
-    //         </div>
-    //     `)
-    // }
-
-    // EXPERIMENTING with ^
-    // renderList(lists) {
-    //     console.log("inside renderList- list component")
-    //     // console.log(lists)
-    //     // let test = document.getElementById('app-container')
-    //     let divListCollection = document.getElementById('all-lists')
-        
-    //     let html = ""
-    //     let x = lists.forEach(list => {
-    //         console.log(list)
-
-    //         return html +=
-    //             (`
-    //                 <div class='card' style="width: 18rem;"> 
-    //                 <div class="card-body">
-    //                 <h5 class="card-title">${this.list.title}</h5>  
-    //                     <ul class="list-group list-group-flush">
-    //                         <li> Example Item </li
-    //                     </ul>
-    //                 </div>
-    //                 </div>
-    //             `)
-    //     })
-    //     // test.parentNode.insertBefore(html, test.nextSibling)
-          
-    //     divListCollection.innerHTML = html
-    // }
