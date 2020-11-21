1675b7e87514b3e7f1f7ea49061bae73c6bcbe2b
no0ro <nrolwes@gmail.com>
2020-11-20 20:20:49 -0500

    added a test form input field and working addItemButton listener

--- list-frontend/src/list.js
+++ list-frontend/src/list.js
@@ -1,3 +1,8 @@
+const listFormFields = `
+    <label for="list-title">List Title:</label>
+    <input type="text" name="title" value="" placeholder="List Title" class="input-text"/>
+ `
+
 class List {
     constructor(title) {
         // this.item = new Item(name)
@@ -5,11 +10,27 @@ class List {
         this.items = [];
         this.lists = [];
     }
+        static newListForm() { 
+            let newFormDiv = document.getElementById('test-list-form')
+            newFormDiv.innerHTML = `
+            <form onsubmit="makeList();">` + 
+                listFormFields + 
+            `
+            <input type="submit" name="submit" value="CreatList" />
+            </form>
+            <br>
+            `
+        }
+
+    }
+
+  
+
 
     // renderItems() {
     //     return this.attributes.items.map(item => item.render()).join('');
     // }
-}
+
 
 function getLists() {
     console.log("inside getLists()")
@@ -20,13 +41,18 @@ function getLists() {
         .then(data => {
             // console.log(data)
             renderListHtml(data)
+            List.newListForm()
         } )
             // console.log(data[1].attributes.title) // "Faith's Wedding: MOH"
             // console.log(data[1].attributes.items[0].name) // "Plan Bachelorette"
         .catch(console.error)
 }
 
+// called after form is submitted -- create a new List
+// inside static new/edit
+function makeList(){
 
+}
 
 // render Lists ALREADY IN DB
 function renderListHtml(listObjects){
@@ -36,26 +62,66 @@ function renderListHtml(listObjects){
     listObjects.forEach((listObj) => {
         console.log(listObj)
 
-        // Grab title
+    // Grab title
         let title = listObj.attributes.title // "Grocery List"
         
-        // <h3> Grocery List </h3>
+    // <h3> Grocery List </h3>
         let h3 = document.createElement('h3')
         h3.innerText = title 
         allLists.append(h3)
 
 
-        // <ul id="items"> </ul> 
+    // <ul id="items"> </ul> 
         let ul = document.createElement('ul')
         ul.className = "items"
         // get all Items from the List Obj, save in arr
         ul.innerHTML = makeArrOfItems(listObj) //taskList.renderTask() // userinput pull from Taskrender
             // append to <li> ^
         allLists.append(ul)
+
+        //   // <ul id="items"> </ul> 
+        //   let ul = document.getElementById('x')
+        // //   ul.className = "items"
+        //   // get all Items from the List Obj, save in arr
+        //   ul.innerHTML = makeArrOfItems(listObj) //taskList.renderTask() // userinput pull from Taskrender
+        //       // append to <li> ^
+        //   //allLists.append(ul)
+
+
+        let input1 = document.createElement('input')
+        input1.setAttribute('type', 'text')
+        input1.setAttribute('id', 'newItemText')
+
+        let inputButton = document.createElement('input')
+        inputButton.setAttribute('type', 'button')
+        inputButton.setAttribute('value', 'Add an Item')
+        inputButton.addEventListener('click', addItemFromButton)
+
+        allLists.append(input1)
+        allLists.append(inputButton)
+
+
+        // <input type="text" id="newItemText">
+        // <input type="button" value="Add an Item" onclick="addItem()">
     
     })
 }
 
+
+function addItemFromButton(e){
+    console.log("inside addItemFromButton")
+    console.log(e)
+    let input = document.getElementById("newItemText");
+    if (input.value.length === 0) {
+        return;
+    }
+    let text = input.value;
+    let newLi = document.createElement("li");
+    newLi.textContent = text;
+    document.getElementById("lists-index").appendChild(newLi);
+    input.value = "";
+
+}
 function renderItems() {
     return this.items.map(item => console.log(item))
 }
