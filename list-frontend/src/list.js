af221fc77c8f35e4804034f22cec92458e2e06b9
no0ro <nrolwes@gmail.com>
2020-11-22 12:43:58 -0500

    stubbed out fetch, create obj, and addtodom

--- list-frontend/src/list.js
+++ list-frontend/src/list.js
@@ -6,5 +6,37 @@ class List {
         this.items = [] 
         // this.lists = [];
     }
-    
+
+
+    createNewItem(name){
+        const newItem = new Item()
+
+    }
+    // 1.
+    getLists() {
+        // fetch
+
+
+        // 2. createListObjects()
+        // 3. addListsToDom()
+    }
+
+
+    // 2 . take data and turn it into List Objects, 
+    //      push to lists array -- will return back to getLists when done
+    createListObjects(){
+        // called from inside fetch 
+        // take in fetch data, pick through hash, assign values
+        // return out  1 list object 
+
+         // if items.length > 1 -- iterare, else, call 
+
+    }
+
+
+    // 3. 
+    addListsToDom() {
+
+    } 
+
 }
