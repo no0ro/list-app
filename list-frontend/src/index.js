a62865929ac0826a239c1384b5a5ca5154957d33
no0ro <nrolwes@gmail.com>
2020-11-22 09:43:38 -0500

    added test data

--- list-frontend/src/index.js
+++ list-frontend/src/index.js
@@ -2,11 +2,17 @@ console.log('index.js - FIRST LINE')
 
 document.addEventListener('DOMContentLoaded', () =>  {
     //  TEST
-    let movie = new List("Movies") // creates movie normally!
-    console.log(movie) 
-    movie.items = new Item("Harry Potter") // ERROR: Uncaught Reference Error Item is not defined
-    console.log(movie) 
+    // let newMovieList = new List("Movies") // creates movie normally!
+    // console.log(newMovieList) 
+    // let newMovie= new Item("Harry Potter") // ERROR: Uncaught Reference Error Item is not defined
+    // let newMovie2= new Item("Superman")
 
+    // console.log(newMovie) 
+    
+    // newMovieList.items.push(newMovie)
+    // newMovieList.items.push(newMovie2)
+    // console.log(newMovieList)
 
+    
     getLists();
 }) 
