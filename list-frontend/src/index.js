68908f15201396be52975e38e99bd5ca54cc197f
no0ro <nrolwes@gmail.com>
2020-11-23 08:09:02 -0500

    added new App call

--- list-frontend/src/index.js
+++ list-frontend/src/index.js
@@ -12,10 +12,35 @@ document.addEventListener('DOMContentLoaded', () =>  {
     // newMovieList.items.push(newMovie)
     // newMovieList.items.push(newMovie2)
     // console.log(newMovieList)
+    // --- TEST END -----
+
+
+    const container = document.querySelector('#app-container')
+    new App(container)
 
     // TO DO - How do i start the app w/o next lines?
   let list = new List()
   list.fetchAndLoadLists()
 
     console.log("back in index.js")
-}) 
+})
+
+// handleFormSubmit(e)
+
+// i dont want the class itself to be listening for the event. 
+// the submit form isnt class specific. its to be used by the app as a whole 
+// the form itself inst in list.js, its in index.html...
+
+
+
+// Form Submit Button Listener.
+// let formSubmitButton = document.getElementById("form-submit")
+// formSubmitButton.addEventListener('click', function (e) {
+//     // `this` = submit ... aka e.target
+//     // inside event listener callback, `this` will be the element that fired the event
+//     e.preventDefault()
+//     console.log(e.target)
+
+//     tempList = new List()
+//     tempList.postList()
+// })
