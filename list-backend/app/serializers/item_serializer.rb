aee92bd492a2934dcca69f3635f485d910908a88
no0ro <nrolwes@gmail.com>
2020-11-23 08:05:29 -0500

    removed created_at attribute

--- list-backend/app/serializers/item_serializer.rb
+++ list-backend/app/serializers/item_serializer.rb
@@ -1,7 +1,7 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
   # specify what attributes we want to use
-  attributes :id, :name, :list_id, :created_at, :list
+  attributes :id, :name, :list_id, :list
   # remove :list if things get weird
 
   belongs_to :list, serializer: ListSerializer
--- list-backend/app/serializers/list_serializer.rb
+++ list-backend/app/serializers/list_serializer.rb
@@ -1,7 +1,7 @@
 class ListSerializer
   include FastJsonapi::ObjectSerializer
   # specify what attributes we want to use
-  attributes :id, :title, :items, :created_at
+  attributes :id, :title, :items
 
   has_many :items, serializer: ItemSerializer
   
