54b54ac2c801e546d6a7a0340ec7fde6033da3d0
no0ro <nrolwes@gmail.com>
2020-11-23 08:10:27 -0500

    added app class to handle submit form event handler

---
+++ list-frontend/src/app.js
@@ -0,0 +1,34 @@
+class App{
+// constructor: provide any custom initialization that must be done before any 
+// other methods can be called on an instantiated object
+
+    constructor(container){
+        this.container = container
+        this.list = new List()
+        this.item = new Item()
+        this.formSubmit = document.getElementById("form-submit") // dont want to bind for EVERY instance. just need to add listener for page document as a whole
+            // ^ before anything else, grab this el 
+        this.bindFormSubmit()
+        // this.list.? = this.bindFormSubmit.bind(this) 
+
+        // this.universe.addMassCallback = this.universeAddMassCallback.bind(this)
+    }   
+
+ 
+    // universeAddMassCallback(){ 
+    //     return this.massSelector.selectedMass
+    // }
+
+    bindFormSubmit() {
+        // let x = document.getElementById("form-submit")
+
+        this.formSubmit.addEventListener("click", function(e) {
+            e.preventDefault()
+            console.log(e.target)
+            alert('I was clicked')
+            this.list.postList()
+        }.bind(this))
+
+    }
+    
+}
