861ddb5bcc86c6cf874344d13db3bd23c501bc7a
no0ro <nrolwes@gmail.com>
2020-11-23 20:37:55 -0500

    changed create method to iterate through multiple item instances at once

--- list-backend/app/controllers/lists_controller.rb
+++ list-backend/app/controllers/lists_controller.rb
@@ -14,17 +14,19 @@ class ListsController < ApplicationController
       list = List.create(list_params) # List(id: integer, title: string, created_at: datetime, updated_at: datetime)
       # > list.title 
         # => "Test4"
-      
+     
       # Review.find_by(id: params[:id]) 
       # [!!] read up on find_or_create_by vs find_by
-    
-      item = Item.find_or_create_by(name: params[:name]) # params[:items].map{|item| dont need to iterate bc will only ever be adding 1 item at a time, despite the items attribute is an array on teh List obj. when submit, its always just 1 item 
+      items = params[:name].map { |itemName| Item.create(name: itemName)}
+      # byebug
+      # item = Item.find_or_create_by(name: params[:name]) 
+      # params[:items].map{|item| dont need to iterate bc will only ever be adding 1 item at a time, despite the items attribute is an array on teh List obj. when submit, its always just 1 item 
       # > Item.find_by(name: params[:name]) 
         # => [["name", "item4"]
 
     
-      #byebug
-      list.items << item # add item to the items array
+   
+      list.items << items # add item to the items array
       # > list.items << item 
         # => [["name", "item 1"], ["list_id", 27], ["created_at", "2020-11-21 15:19:27.893634"], ["updated_at", "2020-11-21 15:19:27.893634"]]
         # => new item obj looks like: [#<Item id: 48, name: "item 1", list_id: 27, created_at: "2020-11-21 15:19:27", updated_at: "2020-11-21 15:19:27">]
